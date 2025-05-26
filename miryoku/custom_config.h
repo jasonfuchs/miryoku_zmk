cc9b043c5e52f37cc5d4c2245a865ee5f8d722d1
Jason Fuchs <jasonfuchs@protonmail.com>
2025-05-26 21:37:25 +0200

    refactor: use extra and tap layers for gaming

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,43 +5,14 @@
 #define MIRYOKU_CLIPBOARD_WIN
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
-#define MIRYOKU_LAYER_GAME \
-&kp TAB,   &kp Q,     &kp W,     &kp E,     &kp R,          &trans,  &trans,   &trans,     &trans, &trans, \
-&kp LSHFT, &kp A,     &kp S,     &kp D,     &kp F,          &trans,  &trans,   &trans,     &trans, &trans, \
-&kp LCTRL, &kp NUM_1, &kp NUM_2, &kp NUM_3, &kp NUM_4,      &trans,  &trans,   &trans,     &trans, &trans, \
-U_NP,      U_NP,      &kp LALT,  &kp SPC,   &mo U_GAMEPLUS, &kp RET, &kp BSPC, &to U_BASE, U_NP,   U_NP
+#define MIRYOKU_LAYER_EXTRA \
+&kp TAB,   &kp Q,     &kp W,     &kp E,     &kp R,     &trans,  &trans,   &trans,     &trans, &trans, \
+&kp LSHFT, &kp A,     &kp S,     &kp D,     &kp F,     &trans,  &trans,   &trans,     &trans, &trans, \
+&kp LCTRL, &kp NUM_1, &kp NUM_2, &kp NUM_3, &kp NUM_4, &trans,  &trans,   &trans,     &trans, &trans, \
+U_NP,      U_NP,      &kp LALT,  &kp SPC,   &mo U_TAP, &kp RET, &kp BSPC, &to U_BASE, U_NP,   U_NP
 
-#define MIRYOKU_LAYER_GAMEPLUS \
+#define MIRYOKU_LAYER_TAP \
 &kp ESC,   &kp G,     &kp W,     &kp X,     &kp T,     &trans,  &trans,   &trans,     &trans, &trans, \
 &kp LSHFT, &kp A,     &kp S,     &kp D,     &kp B,     &trans,  &trans,   &trans,     &trans, &trans, \
 &kp LCTRL, &kp NUM_5, &kp NUM_6, &kp NUM_7, &kp NUM_8, &trans,  &trans,   &trans,     &trans, &trans, \
 U_NP,      U_NP,      &kp LALT,  &kp SPC,   &none,     &kp RET, &kp BSPC, &to U_BASE, U_NP,   U_NP
-
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,     "Base") \
-MIRYOKU_X(EXTRA,    "Extra") \
-MIRYOKU_X(TAP,      "Tap") \
-MIRYOKU_X(BUTTON,   "Button") \
-MIRYOKU_X(NAV,      "Nav") \
-MIRYOKU_X(MOUSE,    "Mouse") \
-MIRYOKU_X(MEDIA,    "Media") \
-MIRYOKU_X(NUM,      "Num") \
-MIRYOKU_X(SYM,      "Sym") \
-MIRYOKU_X(FUN,      "Fun") \
-MIRYOKU_X(GAME,     "Game") \
-MIRYOKU_X(GAMEPLUS, "GamePlus")
-
-#define U_BASE     0
-#define U_EXTRA    1
-#define U_TAP      2
-#define U_BUTTON   3
-#define U_NAV      4
-#define U_MOUSE    5
-#define U_MEDIA    6
-#define U_NUM      7
-#define U_SYM      8
-#define U_FUN      9
-#define U_GAME     10
-#define U_GAMEPLUS 11
-
-#define MIRYOKU_LAYER_EXTRA MIRYOKU_LAYER_GAME 
