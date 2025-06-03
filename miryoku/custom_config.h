cf96264cee9c1824fc16243c7a7e59ee80d2539c
Jason Fuchs <jasonfuchs@protonmail.com>
2025-06-03 19:20:20 +0200

    fix: add &kp to LALT

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -5,10 +5,10 @@
 #pragma once
 
 #define MIRYOKU_LAYER_GAME \
-&kp TAB,   &kp Q, &kp W, &kp E,   &kp R,          &kp Y,   &kp U,    &kp I,      &kp O,   &kp P, \
-&kp LSHFT, &kp A, &kp S, &kp D,   &kp F,          &kp H,   &kp J,    &kp K,      &kp L,   &kp SQT, \
-&kp LCTRL, &kp Z, &kp X, &kp C,   &kp V,          &kp N,   &kp M,    &kp COMMA,  &kp DOT, &kp SLASH, \
-U_NP,      U_NP,  LALT,  &kp SPC, &mo U_GAMEPLUS, &kp RET, &kp BSPC, &to U_BASE, U_NP,    U_NP
+&kp TAB,   &kp Q, &kp W,    &kp E,   &kp R,          &kp Y,   &kp U,    &kp I,      &kp O,   &kp P, \
+&kp LSHFT, &kp A, &kp S,    &kp D,   &kp F,          &kp H,   &kp J,    &kp K,      &kp L,   &kp SQT, \
+&kp LCTRL, &kp Z, &kp X,    &kp C,   &kp V,          &kp N,   &kp M,    &kp COMMA,  &kp DOT, &kp SLASH, \
+U_NP,      U_NP,  &kp LALT, &kp SPC, &mo U_GAMEPLUS, &kp RET, &kp BSPC, &to U_BASE, U_NP,    U_NP
 
 #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 
