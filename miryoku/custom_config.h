b5e8261bf60187387675fbaa5a0e427e829ce291
Jason Fuchs <jasonfuchs@protonmail.com>
2025-12-19 16:28:54 +0100

    Add RALT to game layer and media control to Game+

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,15 +11,15 @@
 &kp TAB,   &kp Q, &kp W,    &kp E,   &kp R,          &kp Y,   &kp U,    &kp I,     &kp O,   &kp P, \
 &kp LSHFT, &kp A, &kp S,    &kp D,   &kp F,          &kp H,   &kp J,    &kp K,     &kp L,   &kp SQT, \
 &kp LCTRL, &kp Z, &kp X,    &kp C,   &kp V,          &kp N,   &kp M,    &kp COMMA, &kp DOT, &kp SLASH, \
-U_NP,      U_NP,  &kp LALT, &kp SPC, &mo U_GAMEPLUS, &kp RET, &kp BSPC, &kp DEL,   U_NP,    U_NP
+U_NP,      U_NP,  &kp LALT, &kp SPC, &mo U_GAMEPLUS, &kp RET, &kp BSPC, &kp RALT,  U_NP,    U_NP
 
 #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 
 #define MIRYOKU_LAYER_GAMEPLUS \
-&kp GRAVE, &kp NUM_7, &kp NUM_8, &kp NUM_9, &kp T, &to U_BASE, &kp F7,    &kp F8,    &kp F9, &kp F12, \
-&kp ESC,   &kp NUM_4, &kp NUM_5, &kp NUM_6, &kp G, U_NU,       &kp F4,    &kp F5,    &kp F6, &kp F11, \
-&kp NUM_0, &kp NUM_1, &kp NUM_2, &kp NUM_3, &kp B, U_NU,       &kp F1,    &kp F2,    &kp F3, &kp F10, \
-U_NP,      U_NP,      U_NA,      U_NA,      U_NA,  &kp RALT,   &kp RCTRL, &kp RSHFT, U_NP,   U_NP
+&kp GRAVE, &kp NUM_7, &kp NUM_8, &kp NUM_9, &kp T, &to U_BASE, &kp F7,   &kp F8,     &kp F9, &kp F12, \
+&kp ESC,   &kp NUM_4, &kp NUM_5, &kp NUM_6, &kp G, U_NU,       &kp F4,   &kp F5,     &kp F6, &kp F11, \
+&kp NUM_0, &kp NUM_1, &kp NUM_2, &kp NUM_3, &kp B, U_NU,       &kp F1,   &kp F2,     &kp F3, &kp F10, \
+U_NP,      U_NP,      U_NA,      U_NA,      U_NA,  &kp C_PREV, &kp C_PP, &kp C_NEXT, U_NP,   U_NP
 
 #define MIRYOKU_LAYERMAPPING_GAMEPLUS MIRYOKU_MAPPING
 
